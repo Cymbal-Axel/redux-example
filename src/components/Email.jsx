d242ada24b404eea9e9da715bd7f86b75126d3c8
Axel <cymbalaxel@gmail.com>
2023-07-25 15:48:29 -0300

    se corrigio un error de tipeo

---
+++ src/components/Email.jsx
@@ -0,0 +1,2 @@
+import { userSlice } from "../redux/userSlice"
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux"
+
+export const Header = () => {
+    const user = useSelector((state)=> state.user)
+    return (
+        <header>
+            <h1>Redux Toolkit Example</h1>
+            <ul>
+                <li>name: {user.name}</li>
+                <li>email: {user.email}</li>
+                <li>username: {user.username}</li>
+            </ul>
+        </header>
+    )
+}
